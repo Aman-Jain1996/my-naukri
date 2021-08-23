2c259e60755b3bcf4ecf7fd463463e6723d46725
Aman Jain <ajain84791@gmail.com>
2021-08-23 21:57:09 +0530

    completed
    Finished applications modal popup

--- src/components/NoApplicants.js
+++ src/components/NoApplicants.js
@@ -2,7 +2,9 @@ import React from 'react'
 
 export default function NoApplicants() {
     return (
-        <>
-        </>
+        <div className="noApplications">
+            <i id="svg" className="fa fa-address-card-o"></i>
+            <p>No applications available</p>   
+        </div>
     )
 }
--- src/components/post.js
+++ src/components/post.js
@@ -2,11 +2,13 @@ import axios from 'axios';
 import React, { useState } from 'react'
 import ReactPaginate from 'react-paginate';
 import { baseUrl } from '../App';
+import NoApplicants from './NoApplicants';
 
 export default function Post(props) {
 
-    const [count, setcount] = useState(0)
-    const [posts,setPosts] = useState(props.posts);
+    const [count, setcount] = useState(0);
+    const [divData, setdivData] = useState("");
+    const [posts,] = useState(props.posts);
     const [pageNumber, setpageNumber] = useState(0);
     let postsPerPage =8;
     let pageVisited = pageNumber * postsPerPage;
@@ -14,9 +16,11 @@ export default function Post(props) {
     function fetchJobPosts(){
         return posts.slice(pageVisited,pageVisited+postsPerPage).map(item => {
              return (<div key={item.id} className="jobPosts-div">
-                 <p style={{display:"none"}} >{item.id}</p>
-                 <p className="postTitle">{item.title}</p>
-                 <p className="postDesc" >{item.description[0].toUpperCase() + item.description.slice(1,) }</p>
+                <div>
+                    <p style={{display:"none"}} >{item.id}</p>
+                    <p className="postTitle">{item.title}</p>
+                    <p className="postDesc" >{item.description[0].toUpperCase()    + item.description.slice(1,) }</p>
+                </div>
                  <div className="location-Div">
                      <p className="postLocation" > <i className="fa fa-map-marker" style={{color:"#2b8cd6",paddingRight:"5px"}}></i>{item.location}</p>
                      <button className="view-Applications" onClick={(e)=>clickHandler(e,item)}>view Applications</button>
@@ -24,6 +28,27 @@ export default function Post(props) {
              </div>)
          })
      }
+
+    function fetchApplications(item){
+        return (<div className="applicationDetails-container">
+            {item.map(data => {
+            return (<div className="applicationDetails" key={data.id}>
+                        <div className="appDetails-header">
+                            <div className="appName">{data.name[0].toUpperCase()}</div>
+                            <div className="appDetails-headerInfo">
+                                <p className="header-name" >{data.name}</p>
+                                <p>{data.email}</p>
+                            </div>
+
+                        </div>
+                        <div className ="appDetails-body" >
+                            <p className="header-name">Skills</p>
+                            <p>{data.skills}</p>
+                        </div>
+                    </div>)
+        })}
+        </div>)
+    }
 
 
     function clickHandler(e,item){
@@ -33,15 +58,13 @@ export default function Post(props) {
 
         axios.get(baseUrl+"/recruiters/jobs/"+item.id+"/candidates",{headers:{"Authorization":localStorage.getItem("userToken")}})
         .then(res => {
-            console.log(res.data)
             if(res.data.message !== undefined){
                 setcount(0);
-                let element = <div className="noApplicants">Aman Jain</div>
-                document.querySelector(".application-container").append(element);
+                setdivData(<NoApplicants />);
             }
             else{
-                setcount(()=>res.data.data.length);
-                console.log(res.data.data.length)
+                setcount(res.data.data.length);
+                setdivData(fetchApplications(res.data.data))
             }
         })
         .catch(err => console.log(err.response))
@@ -83,9 +106,11 @@ export default function Post(props) {
                 </span>
             </div>
             <hr style={{color:"#557DA526",opacity:"0.8"}}/>
-            <p style={{color: "#303F60",margin:"1rem 0",fontSize:"0.8rem"}}>{`Total ${count && 0} applications`}</p>
+            <p style={{color: "#303F60",margin:"1rem 0",fontSize:"0.8rem"}}> 
+                { count === 0 ? `0 applications` : `Total ${count} applications`} 
+            </p>
             <div className="application-container">
-                
+                {divData}
             </div>
         </div>
     </>
